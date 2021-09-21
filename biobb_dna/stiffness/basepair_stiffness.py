5fbf756324198bbd033c8d00220745d62bd2a77e
svletana <luciaa.fabio@gmail.com>
2021-09-21 16:03:08 +0200

    closed plots after saving in basepair_stiffness. started development of canion building block

--- biobb_dna/stiffness/basepair_stiffness.py
+++ biobb_dna/stiffness/basepair_stiffness.py
@@ -189,6 +189,7 @@ class BPStiffness():
         fig.savefig(
             self.io_dict['out']['output_jpg_path'],
             format="jpg")
+        plt.close()
 
         # Remove temporary file(s)
         if self.remove_tmp:
