2e3c9e40853fd1aa9b613a93f8521b32f8906765
gbayarri <genis.bayarri@irbbarcelona.org>
2023-09-08 09:38:49 +0200

    2022.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_dna",
-    version="4.0.0",
+    version="4.1.0",
     author="Biobb developers",
     author_email="lucia.fabio@irbbarcelona.com",
     description="Biobb_dna is a package composed of different analyses for nucleic acid trajectories.",
@@ -19,10 +19,10 @@ setuptools.setup(
     },
     packages=setuptools.find_packages(exclude=['adapters', 'docs', 'test']),
     install_requires=[
-        'biobb_common==4.0.0',
-        'pandas==1.3.0',
+        'biobb_common==4.1.0',
+        'pandas>=1.3.0',
         'scikit-learn==0.24.2'],
-    python_requires='>=3.7,<3.10',
+    python_requires='>=3.8',
     entry_points={
         "console_scripts": [
             "biobb_curves = biobb_dna.curvesplus.biobb_curves:main",
@@ -46,9 +46,9 @@ setuptools.setup(
     },
     classifiers=(
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
